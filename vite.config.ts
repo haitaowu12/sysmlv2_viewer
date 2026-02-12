d092864cc58fc8cf43df3aa301cb563f1334dfe8
haitaowu12 <haitaowu12@gmail.com>
2026-02-12 10:42:21 -0800

    Update base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sysml-viewer/',
+  base: '/sysmlv2_viewer/',
   plugins: [react()],
 })
